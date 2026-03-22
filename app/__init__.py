eaa61d3210b5f5ef74f1bbd138f6ce8e25bea493
Dibya <dibyajyoti22bhanja@gmail.com>
2026-03-22 11:21:50 +0530

    Implement basic HTTP server handler
    Added a simple HTTP server handler to respond to GET requests.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
 from ub_core import BOT, LOGGER, Config, Convo, CustomDB, Message, bot
