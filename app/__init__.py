c229cd6462fd4e8c0742fc850736d54b60df9793
Dibya <dibyajyoti22bhanja@gmail.com>
2026-03-22 11:54:50 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,12 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"OK")
+    def log_message(self, format, *args):
+        pass
+
+threading.Thread(
+    target=lambda: HTTPServer(("0.0.0.0", 8080), Handler).serve_forever(),
+    daemon=True
+).start()
+
 from ub_core import BOT, LOGGER, Config, Convo, CustomDB, Message, bot
